52a08f6067d2cad69271d2c66f3e5dd70cbf065b
Ryker Reed <ryker.reed2024@catalogdna.com>
2025-05-20 13:23:41 -0400

    working login and logout and contact form show hide and redirect

--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,144 @@
 import { createAuth0Client } from '@auth0/auth0-spa-js';
 
-// It should be loaded asynchronously so it doesn't affect page load times.
 const init = async () => {
+  ///////////////Create Auth0 client
   const client = await createAuth0Client({
     clientId: '4CqRLH3KK9xw54bDu8aYcgLSgeA3bku6', //cleintID from auth0
     domain: 'dev-bjajppuyg4p1ypcj.us.auth0.com',//domain from auth0
     authorizationParams: {
-      redirect_uri: 'https://catalog-4006cd-92548d0ba6-2ad8a3acb4830.webflow.io/', //Redirect URL after login
+      redirect_uri: 'https://catalog-4006cd-92548d0ba6-2ad8a3acb4830.webflow.io/services', //Redirect URL after login
     },
   });
 
   console.log(client)
 
+  ///////////////Get buttons
+  const logoutButton = document.getElementById('logout-button');
+  const loginButton = document.getElementById('login-button');
+  const signUpButton = document.getElementById('signup-button');
 
-  // const hidebtn = document.getElementById('hideLink') as HTMLAnchorElement; //query the button
-  // //with the login action
+  const contactStorageButtonActive = document.getElementById('contact-storage-button-active') as HTMLButtonElement;
+  const contactStorageButtonInactive = document.getElementById('contact-storage-button-inactive') as HTMLButtonElement;
+  const contactComputingButtonActive = document.getElementById('contact-computing-button-active') as HTMLButtonElement;
+  const contactComputingButtonInactive = document.getElementById('contact-computing-button-inactive') as HTMLButtonElement;
 
+  const contactStoragePopup = document.getElementById('contact-storage-popup');
+  const contactComputingPopup = document.getElementById('contact-computing-popup');
+
+  ///////////////Redirect after login logic
   const url = new URLSearchParams(window.location.search);
   const code = url.get('code');
 
   if (code) {
-    const { appState } = await client.handleRedirectCallback();
-    const customInfo = appState?.customInfo
-    // await client.handleRedirectCallback(); //handles redirect after successsful login
-    console.log("redirected after login")
-    const user = await client.getUser();
-    console.log(user)
-    console.log("info:")
-    console.log(customInfo)
-    history.replaceState({}, document.title, window.location.origin + window.location.pathname);
+    if (!loginButton || !logoutButton || !signUpButton || !contactStorageButtonActive || !contactStorageButtonInactive || !contactComputingButtonActive || !contactComputingButtonInactive || !contactStoragePopup || !contactComputingPopup) {
+      console.log("button(s) missing")
+    }
+    else {
+      // console.log("redirected after login")
+      const { appState } = await client.handleRedirectCallback();
+      const user = await client.getUser();
+      // console.log(user)
+      const originButtonClicked = appState?.originButtonClicked
+      console.log("info:")
+      console.log(originButtonClicked)
+      if (originButtonClicked == 'contactStorage') {
+        contactStoragePopup.style.display = 'flex'
+      }
+      if (originButtonClicked == 'contactComputing') {
+        contactComputingPopup.style.display = 'flex'
+      }
+      history.replaceState({}, document.title, window.location.origin + window.location.pathname);
+    }
   }
 
-  const logoutBtn = document.getElementById('logOutbtn');
-  const loginUser = document.getElementById('authLogin');
-  const signUpUser = document.getElementById('authSignUp');
-  // const accessDenied = document.querySelector('[Lf-element="deniedEl"]') as HTMLElement;
-  // const accessPage = document.querySelector('[Lf-element="mainPage"]');
+  /////////User Logged-in status
+  const isLoggedIn = await client.isAuthenticated();
 
-  ////////////User authentication
-  const isLoggedIn = await client.isAuthenticated(); //check if user is logged in or not
-
+  ////////////Button show/hide logic
   const showButtons = function () {
-    if (!loginUser || !logoutBtn || !signUpUser) return;
+    if (!loginButton || !logoutButton || !signUpButton || !contactStorageButtonActive || !contactStorageButtonInactive || !contactComputingButtonActive || !contactComputingButtonInactive) return;
 
     if (isLoggedIn) {
-      loginUser.style.visibility = "hidden"
-      logoutBtn.style.visibility = "visible"
-      signUpUser.style.visibility = "hidden"
+      loginButton.style.display = "none"
+      logoutButton.style.display = "inline-block"
+      signUpButton.style.display = "none"
+
+      contactStorageButtonActive.style.display = "block";
+      contactComputingButtonActive.style.display = "block";
+
+      contactStorageButtonInactive.style.display = "none";
+      contactComputingButtonInactive.style.display = "none";
     }
     else if (!isLoggedIn) {
-      loginUser.style.visibility = "visible"
-      logoutBtn.style.visibility = "hidden"
-      signUpUser.style.visibility = "visible"
+      loginButton.style.display = "inline-block"
+      logoutButton.style.display = "none"
+      signUpButton.style.display = "inline-block"
+
+      contactStorageButtonActive.style.display = "none";
+      contactComputingButtonActive.style.display = "none";
+
+      contactStorageButtonInactive.style.display = "block";
+      contactComputingButtonInactive.style.display = "block";
     }
   }
 
   showButtons()
 
-  // const unregisteredUser = function () {
-  //   if (!isLoggedIn) {
-  //     hidebtn.href = ``;
-  //     hidebtn.style.opacity = `50%`;
-  //     accessDenied ? (accessDenied.style.display = `flex`) : '';
-  //   }
-  // };
 
-
-  // const activeUser = function () {
-  //   if (isLoggedIn) {
-  //     hidebtn.href = '<URL that needs to be hidden>';
-  //     hidebtn.classList.remove('disabled');
-  //     accessDenied ? (accessDenied.style.display = `none`) : '';
-  //     accessPage ? accessPage.classList.remove('hide') : '';
-  //   }
-  // };
-
-
-  // unregisteredUser(); //unregistered member
-  // activeUser(); //authenticated user
-
+  /////////////Event listeners on webflow page buttons
   window.Webflow ||= [];
   window.Webflow.push(() => {
 
-    if (!loginUser || !logoutBtn || !signUpUser) return;
+    if (!loginButton || !logoutButton || !signUpButton || !contactStorageButtonInactive || !contactComputingButtonInactive) return;
 
-    loginUser.addEventListener('click', async (e) => {
+    loginButton.addEventListener('click', async (e) => {
       (await client).loginWithRedirect(
         {
           appState: {
-            customInfo: 'value1',
+            originButtonClicked: 'login',
             custom_param2: 'value2',
           },
         }
-      ); //calling the auth0 login function on click
+      );
     });
 
-    ///logout button
-    logoutBtn.addEventListener('click', async () => {
+    logoutButton.addEventListener('click', async () => {
       (await client).logout({
         logoutParams: {
-          returnTo: 'https://catalog-4006cd-92548d0ba6-2ad8a3acb4830.webflow.io/'
+          returnTo: 'https://catalog-4006cd-92548d0ba6-2ad8a3acb4830.webflow.io/services'
         }
       });
     });
 
+    signUpButton.addEventListener('click', async (e) => {
+      console.log("signup clicked")
+    })
+
+    if (!isLoggedIn) {
+      contactStorageButtonInactive.addEventListener('click', async (e) => {
+        (await client).loginWithRedirect(
+          {
+            appState: {
+              originButtonClicked: 'contactStorage',
+              custom_param2: 'value2',
+            },
+          }
+        );
+      })
+
+      contactComputingButtonInactive.addEventListener('click', async (e) => {
+        (await client).loginWithRedirect(
+          {
+            appState: {
+              originButtonClicked: 'contactComputing',
+              custom_param2: 'value2',
+            },
+          }
+        );
+      })
+    }
   });
 };
 
+//////////Initialize
 init();
